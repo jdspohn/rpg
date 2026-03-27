2066a479215f5f8305ca9b467fa1230f36ba1fa9
jdspohn <59989215+jdspohn@users.noreply.github.com>
2026-03-27 14:51:13 -0400

    jumping, air physics, and variable move speed

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -5,20 +5,46 @@ extends CharacterBody3D
 @onready var pivot: Node3D = %Pivot
 @onready var camera_pivot: CameraController = %CameraPivot
 
-var move_direction = Vector3.ZERO
-var move_speed = 4.0
+#Player Stats
+var walk_speed: float = 2.0
+var run_speed: float = 4.0
+var sprint_speed: float = 6.0
+var jump_impulse: float = 13.0
+var fall_acceleration: float = 50.0
 
-func move_and_rotate() -> void:
-	if move_direction != Vector3.ZERO:
-		move_direction = move_direction.rotated(Vector3.UP, camera_pivot.rotation.y)
-		pivot.basis = Basis.looking_at(move_direction)
-	_handle_ground_physics()
-	move_and_slide()
+var move_direction := Vector3.ZERO
+var move_speed: float
 
 func _handle_ground_physics() -> void:
 	velocity.x = move_direction.x * move_speed
 	velocity.z = move_direction.z * move_speed
 
-func _physics_process(_delta: float) -> void:
+func _handle_air_physics(delta) -> void:
+	velocity.y -= fall_acceleration * delta
+	if move_direction != Vector3.ZERO:
+		velocity.x = move_direction.x * move_speed
+		velocity.z = move_direction.z * move_speed
+
+func move_and_rotate(delta) -> void:
+	if move_direction != Vector3.ZERO:
+		move_direction = move_direction.rotated(Vector3.UP, camera_pivot.rotation.y)
+		pivot.basis = Basis.looking_at(move_direction)
+	if is_on_floor():
+		if velocity.y != 0:
+			velocity.y = 0
+		_handle_ground_physics()
+		if input_manager.is_jumping:
+			velocity.y = jump_impulse
+	else:
+		_handle_air_physics(delta)
+	move_and_slide()
+
+func _physics_process(delta: float) -> void:
 	move_direction = Vector3(input_manager.move_input.x, 0, input_manager.move_input.y).normalized()
-	move_and_rotate.call_deferred()
+	if input_manager.move_input.length() <= 0.4:
+		move_speed = walk_speed
+	else:
+		move_speed = run_speed
+	if input_manager.is_sprinting:
+		move_speed = sprint_speed
+	move_and_rotate.call_deferred(delta)
--- Scripts/input_manager.gd
+++ Scripts/input_manager.gd
@@ -7,16 +7,24 @@ extends Node3D
 var move_input = Vector2.ZERO
 var camera_input = Vector2.ZERO
 var gamepad_camera_input = Vector2.ZERO
+var is_sprinting = false
+var is_jumping = false
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _physics_process(_delta: float) -> void:
+	is_sprinting = false
+	is_jumping = false
 	move_input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	gamepad_camera_input = Input.get_vector("look_left", "look_right", "look_up", "look_down")
 	if gamepad_camera_input.length() > 0.1:
 		camera_input.x += gamepad_camera_input.y * gamepad_sensitivity
 		camera_input.y += gamepad_camera_input.x * gamepad_sensitivity
+	if Input.is_action_pressed("sprint"):
+		is_sprinting = true
+	if Input.is_action_just_pressed("jump"):
+		is_jumping = true
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
