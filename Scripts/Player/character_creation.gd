639e7a26436771e7a0d7c21edf0e5e32a043e38d
jdspohn <59989215+jdspohn@users.noreply.github.com>
2026-05-01 13:50:21 -0400

    added checks for character creation input to disable creation

--- Scripts/Player/character_creation.gd
+++ Scripts/Player/character_creation.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
 
 @onready var pivot: Node3D = %Pivot
-@onready var input_name: LineEdit = $Control/InputName
+@onready var input_name: LineEdit = %InputName
+@onready var label_no_spaces: Label = %LabelNoSpaces
+
 @onready var button_create: Button = %ButtonCreate
 @onready var button_back: Button = %ButtonBack
 
@@ -10,6 +12,9 @@ var character_name: String
 func _ready() -> void:
 	button_create.pressed.connect(_on_create_pressed)
 	button_back.pressed.connect(_on_back_pressed)
+	input_name.text_changed.connect(_on_text_changed)
+	button_create.disabled = true
+	label_no_spaces.visible = false
 
 
 func _physics_process(_delta: float) -> void:
@@ -20,8 +25,18 @@ func _rotate_character() -> void:
 	pass
 
 
+func _on_text_changed(new_text) -> void:
+	label_no_spaces.visible = false
+	if input_name.text == "" or new_text.contains(" "):
+		button_create.disabled = true
+		if new_text.contains(" "):
+			label_no_spaces.visible = true
+	else:
+		button_create.disabled = false
+
+
 func _on_create_pressed() -> void:
-	if input_name.text == "":
+	if input_name.text == "" or input_name.text.contains(" "):
 		return
 	character_name = input_name.text
 	print ("user://playersaves/", character_name)
