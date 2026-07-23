88ca17f0d083d2214946b4656d987574dd7c19d0
jdspohn <spohnjd@gmail.com>
2026-07-23 11:01:49 -0400

    added npc data to map script

--- Scripts/Local Server/map.gd
+++ Scripts/Local Server/map.gd
@@ -1,9 +1,24 @@
 extends Node
 
+var map_npcs = [
+	{
+		position = Vector3(-15,0,17),
+		npc_id = "wolf"
+	},
+	{
+		position = Vector3(19,17,-28),
+		npc_id = "skeleton"
+	},
+	{
+		position = Vector3(15,17,-42),
+		npc_id = "skeleton"
+	}
+]
+var active_npcs = []
 
 func _ready() -> void:
 	pass
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
